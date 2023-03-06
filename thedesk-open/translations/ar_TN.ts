c58e20a89084b39089335131d6bdecf823f782ba
Victor Tran <vicr12345@gmail.com>
2023-03-06 21:56:52 +1100

    Update to Qt 6

--- thedesk-open/translations/ar_TN.ts
+++ thedesk-open/translations/ar_TN.ts
@@ -5,37 +5,45 @@
     <name>FileAssociationSelect</name>
     <message>
         <location filename="../fileassociationselect.ui" line="14"/>
+        <location filename="../../../build-thedesk-xdg-utils-Chroot_6-Debug/thedesk-open/thedesk-open_autogen/include/ui_fileassociationselect.h" line="253"/>
         <source>Select App</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../fileassociationselect.ui" line="61"/>
+        <location filename="../../../build-thedesk-xdg-utils-Chroot_6-Debug/thedesk-open/thedesk-open_autogen/include/ui_fileassociationselect.h" line="254"/>
         <source>Open File</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../fileassociationselect.ui" line="81"/>
+        <location filename="../../../build-thedesk-xdg-utils-Chroot_6-Debug/thedesk-open/thedesk-open_autogen/include/ui_fileassociationselect.h" line="255"/>
         <source>Choose an app</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../fileassociationselect.ui" line="95"/>
         <location filename="../fileassociationselect.ui" line="281"/>
+        <location filename="../../../build-thedesk-xdg-utils-Chroot_6-Debug/thedesk-open/thedesk-open_autogen/include/ui_fileassociationselect.h" line="256"/>
+        <location filename="../../../build-thedesk-xdg-utils-Chroot_6-Debug/thedesk-open/thedesk-open_autogen/include/ui_fileassociationselect.h" line="259"/>
         <source>Cancel</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../fileassociationselect.ui" line="134"/>
+        <location filename="../../../build-thedesk-xdg-utils-Chroot_6-Debug/thedesk-open/thedesk-open_autogen/include/ui_fileassociationselect.h" line="257"/>
         <source>Multiple Apps</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../fileassociationselect.ui" line="274"/>
+        <location filename="../../../build-thedesk-xdg-utils-Chroot_6-Debug/thedesk-open/thedesk-open_autogen/include/ui_fileassociationselect.h" line="258"/>
         <source>Show All Apps</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../fileassociationselect.ui" line="307"/>
+        <location filename="../../../build-thedesk-xdg-utils-Chroot_6-Debug/thedesk-open/thedesk-open_autogen/include/ui_fileassociationselect.h" line="260"/>
         <source>All Apps</source>
         <translation type="unfinished"></translation>
     </message>
@@ -83,21 +91,25 @@ You can use %2 again, grab a new app to open this file, or you can try opening i
     <name>SetDefaultPopover</name>
     <message>
         <location filename="../setdefaultpopover.ui" line="14"/>
+        <location filename="../../../build-thedesk-xdg-utils-Chroot_6-Debug/thedesk-open/thedesk-open_autogen/include/ui_setdefaultpopover.h" line="78"/>
         <source>Form</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../setdefaultpopover.ui" line="35"/>
+        <location filename="../../../build-thedesk-xdg-utils-Chroot_6-Debug/thedesk-open/thedesk-open_autogen/include/ui_setdefaultpopover.h" line="79"/>
         <source>Set Default?</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../setdefaultpopover.ui" line="42"/>
+        <location filename="../../../build-thedesk-xdg-utils-Chroot_6-Debug/thedesk-open/thedesk-open_autogen/include/ui_setdefaultpopover.h" line="80"/>
         <source>TextLabel</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../setdefaultpopover.ui" line="62"/>
+        <location filename="../../../build-thedesk-xdg-utils-Chroot_6-Debug/thedesk-open/thedesk-open_autogen/include/ui_setdefaultpopover.h" line="81"/>
         <source>Just this time</source>
         <translation type="unfinished"></translation>
     </message>
